7109f1b1c2cc985ba4e1c05d2434c2084b5c8245
Raelene Thomas <133934127+thory035@users.noreply.github.com>
2024-03-12 16:37:11 +1030

    Input tick()
    Commented specified requirements for Item functions. Input code for tick(). Added getTick() -- just in case.
    Other: input comment note for Food class.

--- src/students/items/Food.java
+++ src/students/items/Food.java
@@ -1,5 +1,6 @@
 package students.items;
 
+//sub class of Item - should not be able to be instantiated
 public class Food {
 
 
--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -1,6 +1,21 @@
 package students.items;
 
 public class Item {
-
+	// tracks age (start at 0)
+		private int age = 0;
+		
+	public void tick() {
+		age++; 
+	}
+	
+	public int getTick() {
+		return age;
+	}
+	
+	// maturation age
+	
+	// death age
+	
+	// monetary value 
 	
 }
