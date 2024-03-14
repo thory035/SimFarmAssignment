47a094242cb90f3f72d69f46acf1269aa22d97cd
Raelene Thomas <133934127+thory035@users.noreply.github.com>
2024-03-15 02:23:48 +1030

    Update Soil.java
    Creation of Soil class replicated from UntilledSoil class. Removed cost from UntilledSoil and Weed classes.

--- src/students/items/Soil.java
+++ src/students/items/Soil.java
@@ -1,5 +1,14 @@
 package students.items;
 
-public class Soil {
+public class Soil extends Item {
 
+	// superclass constructor
+	public Soil() {
+		super(Integer.MAX_VALUE, Integer.MAX_VALUE, 0);
+	}
+	
+	@Override
+	public String toString() {
+		return ".";
+	}
 }
--- src/students/items/UntilledSoil.java
+++ src/students/items/UntilledSoil.java
@@ -2,7 +2,6 @@ package students.items;
 
 public class UntilledSoil extends Item {
 	
-	public static int cost = -1;
 	// superclass constructor
 	public UntilledSoil() {
 		super(Integer.MAX_VALUE, Integer.MAX_VALUE, -2);
