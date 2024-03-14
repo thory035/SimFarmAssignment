8fb4098572e82fe64ff955216e4aa163dbc32a5f
Raelene Thomas <133934127+thory035@users.noreply.github.com>
2024-03-15 00:19:37 +1030

    Update Food.java
    Food class is made abstract. Added constructor with super().

--- src/students/items/Food.java
+++ src/students/items/Food.java
@@ -1,7 +1,9 @@
 package students.items;
 
 //sub class of Item - should not be able to be instantiated
-public class Food {
-
-
+public abstract class Food extends Item {
+	
+	 public Food(int maturAge, int deathAge, int value) {
+		 super(maturAge, deathAge, value);
+	 }
 }
