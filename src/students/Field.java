7ce46168470cea17f574d42744389b8598cd3cde
Raelene Thomas <133934127+thory035@users.noreply.github.com>
2024-03-15 02:44:21 +1030

    Update Field.java
    Created Field class. Added array to represent field size.

--- src/students/Field.java
+++ src/students/Field.java
@@ -1,10 +1,12 @@
 package students;
 
 
-public class Field {
+public class Field extends Item {
+	// array of items to represent the field size
+	private Item[][] field;
 	
-	public Field(int height, int width)
-	{
+	public Field(int height, int width) {
+		this.field = new Item[height][width];
 	}
 	
 }
