53e5f8724ed9b3e237329eeebd5ca6843dbb041d
Raelene Thomas <133934127+thory035@users.noreply.github.com>
2024-03-15 00:48:44 +1030

    Update Grain.java
    Created constructor super. Added generationCount and getter. Added toString().

--- src/students/items/Grain.java
+++ src/students/items/Grain.java
@@ -1,6 +1,23 @@
 package students.items;
 
-public class Grain{
-
+public class Grain extends Food{
 	
+	private static int generationCount = 0;
+	
+	public Grain() {
+		super(2, 6, 2);
+		
+		generationCount++;
+	}
+	
+	public String toString() {
+		if (age < maturAge) {
+			return"G";
+		} else {
+			return "g";
+		}
+		
+	public static int getGenerationCount() {
+		return generationCount;
+	}
 }
