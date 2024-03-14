e53a21ea07fa14b7d4d7eb0b266b6ed23c2432b9
Raelene Thomas <133934127+thory035@users.noreply.github.com>
2024-03-15 02:11:12 +1030

    Update Weed.java
    Creation of Weed class through replicating Apple Class. Adjusted code to requirements.

--- src/students/items/Weed.java
+++ src/students/items/Weed.java
@@ -1,6 +1,15 @@
 package students.items;
 
-public class Weed
-{
-
+public class Weed extends Item {
+		
+		public static int cost = -1;
+		// superclass constructor
+		public Weed() {
+			super(Integer.MAX_VALUE, Integer.MAX_VALUE, -2);
+		}
+		
+		@Override
+		public String toString() {
+			return "#";
+		}
 }
