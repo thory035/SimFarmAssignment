76a03a0792c35199953c97c7daac9f1bc19c122c
Raelene Thomas <133934127+thory035@users.noreply.github.com>
2024-03-15 02:17:29 +1030

    Update UntilledSoil.java
    Created UntilledSoil class replicated from Weed class. Adjusted code according to requirements. Formatted Weed class.

--- src/students/items/UntilledSoil.java
+++ src/students/items/UntilledSoil.java
@@ -1,6 +1,15 @@
 package students.items;
 
-public class UntilledSoil {
-
-
+public class UntilledSoil extends Item {
+	
+	public static int cost = -1;
+	// superclass constructor
+	public UntilledSoil() {
+		super(Integer.MAX_VALUE, Integer.MAX_VALUE, -2);
+	}
+	
+	@Override
+	public String toString() {
+		return "/";
+	}
 }
--- src/students/items/Weed.java
+++ src/students/items/Weed.java
@@ -2,14 +2,14 @@ package students.items;
 
 public class Weed extends Item {
 		
-		public static int cost = -1;
-		// superclass constructor
-		public Weed() {
-			super(Integer.MAX_VALUE, Integer.MAX_VALUE, -2);
-		}
-		
-		@Override
-		public String toString() {
-			return "#";
-		}
+	public static int cost = -1;
+	// superclass constructor
+	public Weed() {
+		super(Integer.MAX_VALUE, Integer.MAX_VALUE, -2);
+	}
+	
+	@Override
+	public String toString() {
+		return "#";
+	}
 }
