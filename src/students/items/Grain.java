18e9fae9422e312795eb0019a99bc10e624803fb
Raelene Thomas <133934127+thory035@users.noreply.github.com>
2024-03-15 01:04:13 +1030

    Update Grain.java
    Added formatting and comments.

--- src/students/items/Grain.java
+++ src/students/items/Grain.java
@@ -1,22 +1,26 @@
 package students.items;
 
 public class Grain extends Food{
-	
+	// counter to track Grain instances
 	private static int generationCount = 0;
 	
+	public static int cost = 1;
+	// superclass constructor
 	public Grain() {
 		super(2, 6, 2);
-		
+		// counter: incremental(1)
 		generationCount++;
 	}
 	
+	@Override
 	public String toString() {
 		if (age < maturAge) {
 			return"G";
 		} else {
 			return "g";
 		}
-		
+	}
+	// gets total number of grain instances	
 	public static int getGenerationCount() {
 		return generationCount;
 	}
