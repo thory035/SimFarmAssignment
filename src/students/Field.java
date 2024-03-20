11c45c6be940e47a15c4a09463289eab9f367b28
Raelene Thomas <133934127+thory035@users.noreply.github.com>
2024-03-20 14:19:35 +1030

    Field.java - Tick()
    Forgot what I was doing for this class so modified variables for readability (row and coloumn). Added tick() function.

--- src/students/Field.java
+++ src/students/Field.java
@@ -11,10 +11,24 @@ public class Field {
 	public Field(int height, int width) {
 		this.field = new Item[height][width];
 		// add new Soil to Field under conditions 
-		for(int i = 0; i < height; i++) {
-			for (int k = 0; k < width; k++) {
+		for(int row = 0; row < height; row++) {
+			for (int column = 0; column < width; column++) {
 				// using import to add soil
-				this.field[i][k] = new Soil();
+				this.field[row][column] = new Soil();
+			}
+		}
+	}
+
+	public void tick() {
+		// iterate each row
+		for(int row = 0; row < field.length; row++) {
+			// iterate each column
+			for(int column = 0; column < field[row].length; column++) {
+				// check if not null
+				if (field[row][column] != null) {	
+					// increase age++
+					field[row][column].tick();
+				}
 			}
 		}
 	}
