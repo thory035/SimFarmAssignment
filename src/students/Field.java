7291969c705982e185b946d2743e9853692f39e2
Raelene Thomas <133934127+thory035@users.noreply.github.com>
2024-03-21 23:59:15 +1030

    Update Field.java
    I realised I needed to have the following dot points after tick() all within a single block of code. Imported item objects for utilisation. Removed commented notes.

--- src/students/Field.java
+++ src/students/Field.java
@@ -1,7 +1,11 @@
 package students;
 
 import students.items.Item;
-import students.items.Soil;
+import students.items.Soil; 
+import students.items.Weed;
+import students.items.UntilledSoil;
+
+import java.lang.Math;
 
 public class Field {
 	// array of items to represent the field size
@@ -24,20 +28,24 @@ public class Field {
 		for(int row = 0; row < field.length; row++) {
 			// iterate each column
 			for(int column = 0; column < field[row].length; column++) {
+				// refers to the instance of Item and assign to variable	
+				Item fieldItem = field[row][column];
 				// check if not null
-				if (field[row][column] != null) {	
-					// increase age++
-					field[row][column].tick();
+				if (fieldItem != null) {	
+					// calls upon method from Item class
+					fieldItem.tick();
+					// if this is a Soil object and there is a 20% chance
+					if (fieldItem instanceof Soil && Math.random() < 0.2) {
+						// this location becomes a Weed
+						field[row][column] = new Weed();
+					}
+					// otherwise if fieldItem died
+					else if (fieldItem.died()) {
+						// this location becomes UntilledSoil
+						field[row][column] = new UntilledSoil();
+						}
+					}
 				}
 			}
 		}
 	}
-	
-	// initialise function
-			// for the index at row position
-			// for the index at column position
-				// if at row|column position is not null and soil(instanceof)
-					// calculate random number, check if less than 20% (0.2)
-					// need to import Math.random()??
-					// if true replace soil with weed (new Weed)
-}
