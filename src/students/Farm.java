6b64ab71bdac7642e4b37a07731fae8f527f5107
Raelene Thomas <133934127+thory035@users.noreply.github.com>
2024-03-29 20:36:22 +1030

    Update Farm.java
    Initialised farm dimensions. Added to run().

--- src/students/Farm.java
+++ src/students/Farm.java
@@ -5,19 +5,26 @@ import java.ulti.Scanner;
 public class Farm {
 	
 	// initialise Field somehow..
+	private Field field;
 	// balance
 	private int bankBalance;
 	
 	// initialise farm with appropriate size and player money
 	public Farm(int fieldWidth, int fieldHeight, int startingFunds)
 	{
-		
+		this.field = new Field(fieldHeight, fieldWidth);
+		this.bankBalance = startingFunds;
 	}
 	
 	// run the farm simulation
 	public void run()
 	{
 		Scanner scanner = new Scanner(System.in);
+		System.out.println(field);
+		System.out.println("Bank balance: $" + bankBalance);
+		System.out.println("Enter your next action: ";
+		
+		
 	}
 	
 }
