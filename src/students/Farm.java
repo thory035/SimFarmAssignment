4de7f448e42fb4ea5b2263c19d18b395455dd87c
Raelene Thomas <133934127+thory035@users.noreply.github.com>
2024-03-30 00:41:18 +1030

    Update Farm.java - run()
    Added structure to run().  Corrected position of scanner code.

--- src/students/Farm.java
+++ src/students/Farm.java
@@ -4,10 +4,11 @@ import java.ulti.Scanner;
 
 public class Farm {
 	
-	// initialise Field somehow..
+	// initialise Field 
 	private Field field;
 	// balance
 	private int bankBalance;
+	Scanner scanner = new Scanner(System.in);
 	
 	// initialise farm with appropriate size and player money
 	public Farm(int fieldWidth, int fieldHeight, int startingFunds)
@@ -15,16 +16,67 @@ public class Farm {
 		this.field = new Field(fieldHeight, fieldWidth);
 		this.bankBalance = startingFunds;
 	}
-	
 	// run the farm simulation
-	public void run()
-	{
-		Scanner scanner = new Scanner(System.in);
+	public void run() {
+		
+		// display field
 		System.out.println(field);
 		System.out.println("Bank balance: $" + bankBalance);
-		System.out.println("Enter your next action: ";
+		System.out.println("Enter your next action: "
+				+ "t x y: till"
+				+ "h x y: harvest"
+				+ "p x y: plant"
+				+ "s: field summary"
+				+ "w: wait"
+				+ "q: quit");
+		
+		String input = scanner.nextLine();
+		if ("q".equals(input)) 
+			break; 
+		
+		//separate inputs as individual items(coordinates) when spaces exist
+		String[] cells = input.split(" ");
+		int x, y;
 		
 		
+		// actions - try, switch, case for options
+		try {
+			switch ([0]) {
+				// till
+				case "t":
+				x = ;
+				y = ;
+
+					break;
+				// harvest
+				case "h"
+				x = ;
+				y = ;
+					break;
+				
+				// plant
+				case "p"
+					break;
+					
+				// summary
+				case "w"
+					System.out.println(field.getSummary());
+					break;
+				
+				// wait
+				case "w"
+					break;
+				
+				default:
+					System.out.println("Error. Try again.");
+					break;
+				
+			}
+			
+			
+		}
+		
+		//should add exceptions for invalid field coordinates
 	}
 	
 }
